cd9820213911a55319d8606cd4ee204d5c66c3ea
Joedson Ferreira <95484503+devjoedson91@users.noreply.github.com>
2024-05-14 13:44:51 -0300

    feat - login screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,119 @@
+"use client";
+import { useContext, useEffect, useState } from "react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Image from "next/image";
+import { Auth } from "@/hooks/auth";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Eye, EyeOff } from "lucide-react";
+
+const formSchema = z.object({
+  email: z.string().email({ message: "Informe um email válido" }),
+  password: z
+    .string()
+    .min(4, { message: "A senha deve ter no mínimo de 4 caracteres" }),
+});
+
 export default function Home() {
-  return <h1>SignIn</h1>;
+  const form = useForm<z.infer<typeof formSchema>>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    resolver: zodResolver(formSchema),
+  });
+
+  const { signIn } = useContext(Auth);
+
+  const [passwordIsVisible, setPasswordIsVisible] = useState(false);
+
+  const [loading, setLoading] = useState(false);
+
+  async function handleSubmit(data: z.infer<typeof formSchema>) {
+    setLoading(true);
+
+    await signIn(data);
+
+    setLoading(false);
+  }
+
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleSubmit)}
+          className="flex flex-col items-center gap-8"
+        >
+          {/* <Image src={Logo} alt="Logo" className="h-16 w-48" /> */}
+          <h1 className="text-2xl self-start font-bold">Login</h1>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    placeholder="E-mail"
+                    className="h-12 w-80 text-sm font-semibold"
+                    {...field}
+                  />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <div className="relative">
+                  <FormControl>
+                    <Input
+                      placeholder="Senha"
+                      className="h-12 w-80 text-sm font-semibold"
+                      type={passwordIsVisible ? "text" : "password"}
+                      {...field}
+                    />
+                  </FormControl>
+                  <Button
+                    type="button"
+                    size="icon"
+                    className="absolute right-0 top-0 h-full border bg-white hover:bg-transparent"
+                    onClick={() => setPasswordIsVisible(!passwordIsVisible)}
+                  >
+                    {passwordIsVisible ? (
+                      <Eye size={18} color="#000" />
+                    ) : (
+                      <EyeOff size={18} color="#000" />
+                    )}
+                  </Button>
+                </div>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button
+            type="submit"
+            className="bg-mainGreen hover:bg-mainGreen/60 h-12 uppercase tracking-widest w-80 rounded-md text-sm text-white outline-none"
+            disabled={loading}
+          >
+            Entrar
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,12 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
@@ -18,6 +18,13 @@ const config = {
       },
     },
     extend: {
+      colors: {
+        mainGreen: "#007D21",
+        yelowDescription: "#F88B0C",
+        gray100: "#EAECF0",
+        gray500: "#868C94",
+        grayBorder: "#DDE4EE",
+      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
@@ -27,14 +34,19 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "activity-indicator": {
+          from: { trasform: "rotate(0deg)" },
+          to: { transform: "rotate(360deg)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "activity-indicator": "activity-indicator 0.7s linear infinite",
       },
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
