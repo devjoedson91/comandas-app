b0438da4902f3179aa28e7a00dd21b6d7f838a1c
Joedson Ferreira <95484503+devjoedson91@users.noreply.github.com>
2024-06-03 09:43:49 -0300

    update page.tsx

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -80,7 +80,7 @@ export default function Menu() {
   return (
     <div>
       <MenuHeader />
-      <div className="p-5 flex flex-col gap-7">
+      <div className="p-6 flex flex-col gap-7">
         <h1 className="font-base text-base font-semibold">Categorias</h1>
         <div className="flex w-full gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
           {category.map((item) => {
